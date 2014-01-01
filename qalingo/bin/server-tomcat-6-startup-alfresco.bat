d97dcb59aafd5b3ebbefa67c6f6977f623692762
denis <denis@Hoteia-Laptop.home>
2014-01-01 14:05:20 +0100

    alfresco

--- qalingo/bin/server-tomcat-6-startup-alfresco.bat
+++ qalingo/bin/server-tomcat-6-startup-alfresco.bat
@@ -1,8 +1,16 @@
 CALL %~dp0env.bat
 
-SET PORT_PREFIX=41
-SET PROJECT_PATH=%~dp0../workspace/git/qalingo/parent-hoteia-qalingo
-SET JAVA_OPTS=-Xmx1024m -XX:MaxPermSize=256m -Dport.prefix=%PORT_PREFIX% -Dproject.path=%PROJECT_PATH% -Xdebug -Xrunjdwp:transport=dt_socket,address=%PORT_PREFIX%000,server=y,suspend=n 
+SET JAVA_HOME=C:\dev\tools\jdk1.7.0_45
+SET JRE_HOME=%JAVA_HOME%/jre
+
+SET PORT_PREFIX=8
+REM SET PROJECT_PATH=%~dp0../workspace/git/qalingo-alfresco/alfresco-allinone
+SET PROJECT_PATH=C:/dev/hoteia/qalingo_merge/workspace/qalingo-alfresco/alfresco-allinone
+
+SET SOLR_HOME_DIR=%~dp0../workspace/git/qalingo-alfresco/alfresco-allinone/solr/solr_home
+SET SOLR_DATA_DIR=%SOLR_HOME_DIR%/data
+
+SET JAVA_OPTS=-Xmx1024m -XX:MaxPermSize=256m -Dport.prefix=%PORT_PREFIX% -Dproject.path=%PROJECT_PATH% -Dsolr.home.dir=%SOLR_HOME_DIR% -Dsolr.data.dir=%SOLR_DATA_DIR% -Xdebug -Xrunjdwp:transport=dt_socket,address=%PORT_PREFIX%000,server=y,suspend=n 
 
 SET CATALINA_HOME=%~dp0../servers/server-apache-tomcat-6.0.35-alfresco
 REM SET CATALINA_OPTS=
