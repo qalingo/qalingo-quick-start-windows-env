caafff0d2237b5b3ef08b73213a68e02d9a63aed
denis <denis@Hoteia-Laptop>
2014-09-06 19:32:59 +0200

    remore-rest -> remote-rest-frontoffice

--- qalingo/bin/server-tomcat-startup-remote-rest-frontoffice.bat
+++ qalingo/bin/server-tomcat-startup-remote-rest-frontoffice.bat
@@ -1,10 +1,10 @@
 CALL %~dp0env.bat
 
 SET PORT_PREFIX=22
-SET PROJECT_PATH=%~dp0../workspace/git/qalingo-web-remote
+SET PROJECT_PATH=%~dp0../workspace/git/qalingo-web-remote-frontoffice
 SET JAVA_OPTS=-Xmx1024m -XX:MaxPermSize=256m -Dport.prefix=%PORT_PREFIX% -Dproject.path=%PROJECT_PATH% -Xdebug -Xrunjdwp:transport=dt_socket,address=%PORT_PREFIX%000,server=y,suspend=n 
 
-SET CATALINA_HOME=%~dp0../servers/server-apache-tomcat-7.0.22-remote-rest
+SET CATALINA_HOME=%~dp0../servers/server-apache-tomcat-7.0.22-remote-rest-frontoffice
 REM SET CATALINA_OPTS=
 
 SET PATH=%PATH%;%CATALINA_HOME%\bin;
